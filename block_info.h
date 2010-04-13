706f2ca30bf0f8bf795a24434def4be1b603d320
Hubert Kario <kasparov1@wp.pl>
2010-04-13 00:33:00 +0000

    fix license information

--- block_info.h
+++ block_info.h
@@ -1,3 +1,25 @@
+/** hdck - hard drive low-level error and badsector checking 
+ * 
+ * Copyright (C) 2010  Hubert Kario
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * uses parts of code from GNU dd 
+ * Copyright (C) 2002-9 Bruce Allen <smartmontools-support@lists.sourceforge.net>
+ * Copyright (C) 2008-9 Christian Franke <smartmontools-support@lists.sourceforge.net>
+ * Copyright (C) 2000 Michael Cornwell <cornwell@acm.org>
+ */
 #ifndef __BLOCK_INFO_H
 #define __BLOCK_INFO_H 1
 
--- hdck.c
+++ hdck.c
@@ -1,29 +1,24 @@
-/** hdck - hard drive low-level errors checking 
- * Compile with GCC, and -lrt -lm
- * (c) 2009 Hubert Kario
- * uses parts of code from GNU dd:
- *  * Copyright (C) 2002-9 Bruce Allen <smartmontools-support@lists.sourceforge.net>
- *  * Copyright (C) 2008-9 Christian Franke <smartmontools-support@lists.sourceforge.net>
- *  * Copyright (C) 2000 Michael Cornwell <cornwell@acm.org>
- *  *
- *  * This program is free software; you can redistribute it and/or modify
- *  * it under the terms of the GNU General Public License as published by
- *  * the Free Software Foundation; either version 2, or (at your option)
- *  * any later version.
- *  *
- *  * You should have received a copy of the GNU General Public License
- *  * (for example COPYING); if not, write to the Free
- *  * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *  *
- *  * This code was originally developed as a Senior Thesis by Michael Cornwell
- *  * at the Concurrent Systems Laboratory (now part of the Storage Systems
- *  * Research Center), Jack Baskin School of Engineering, University of
- *  * California, Santa Cruz. http://ssrc.soe.ucsc.edu/
+/** hdck - hard drive low-level error and badsector checking 
+ * 
+ * Copyright (C) 2010  Hubert Kario
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free software Foundation; wither version 3, or (at your opinion)
- * any later version.
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * uses parts of code from GNU dd 
+ * Copyright (C) 2002-9 Bruce Allen <smartmontools-support@lists.sourceforge.net>
+ * Copyright (C) 2008-9 Christian Franke <smartmontools-support@lists.sourceforge.net>
+ * Copyright (C) 2000 Michael Cornwell <cornwell@acm.org>
  */
 
 #define _XOPEN_SOURCE 500
